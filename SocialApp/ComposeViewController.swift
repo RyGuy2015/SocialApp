7d626f9b37e09b415e4e91cc7f43026cc545f5be
RyGuy2015 <ry.ingram01@gmail.com>
2016-03-05 11:24:37 -0500

    Updates to Social App

--- SocialApp/ComposeViewController.swift
+++ SocialApp/ComposeViewController.swift
@@ -11,13 +11,13 @@ import Accounts
 import Social
 
 class ComposeViewController: UIViewController, UITextViewDelegate {
+    
     var selectedAccount : ACAccount!
     
     @IBOutlet weak var tweetContent: UITextView!
-    @IBOutlet weak var postActivity: UIActivityIndicatorView!
     @IBOutlet weak var postButton: UIButton!
-
-
+    @IBOutlet weak var postActivity: UIActivityIndicatorView!
+    
     @IBAction func dismissView(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
@@ -26,52 +26,54 @@ class ComposeViewController: UIViewController, UITextViewDelegate {
         postContent(self.tweetContent.text)
     }
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         self.tweetContent.delegate = self
+    }
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        let targetLength : Int = 140
+        
+        // The function countElements() was replaced with count()
+        // Now, access the characters property of the String, then call count on that.
+        return textView.text.characters.count <= targetLength
+        
     }
     
     func postContent(post : String) {
         postActivity.startAnimating()
         
         if let account = selectedAccount {
-            let requestURL = NSURL(string: "https:// api.twitter.com/ 1.1/ statuses/ update.json")
-            let request = SLRequest(forServiceType: SLServiceTypeTwitter, requestMethod: SLRequestMethod.POST, URL: requestURL, parameters: NSDictionary(object: post, forKey: "status"))
+            let requestURL = NSURL(string: "https://api.twitter.com/1.1/statuses/update.json")
+            let request = SLRequest(forServiceType: SLServiceTypeTwitter,
+                                    requestMethod: SLRequestMethod.POST,
+                                    URL: requestURL,
+                                    parameters: NSDictionary(object: post, forKey: "status") as [NSObject : AnyObject])
             
             request.account = account
             
-            request.performRequestWithHandler()
-            {
+            request.performRequestWithHandler() {
                 responseData, urlResponse, error in
-                if( urlResponse.statusCode = = 200)
+                
+                if(urlResponse.statusCode == 200)
                 {
-                    print(" Status Posted")
+                    print("Status Posted")
                     
-                    dispatch_async( dispatch_get_main_queue())
+                    dispatch_async(dispatch_get_main_queue())
                     {
                         self.postActivity.stopAnimating()
-                        self.dismissViewControllerAnimated( true, completion: nil)
+                        self.dismissViewControllerAnimated(true, completion: nil)
                     }
                 }
             }
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
--- SocialApp/TweetViewController.swift
+++ SocialApp/TweetViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class TweetViewController: UIViewController {
+    
+    
     @IBOutlet weak var tweetAuthorAvatar: UIImageView!
-    @IBOutlet weak var tweetAuthorName: UIImageView!
+    @IBOutlet weak var tweetAuthorName: UILabel!
     @IBOutlet weak var tweetText: UITextView!
-
     @IBAction func dismissView(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
@@ -21,34 +22,24 @@ class TweetViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let userData = selectedTweet?.objectForKey(" user") as! NSDictionary
         
-        tweetText.text? = selectedTweet?.objectForKey("text") as! NSString
+        let userData = selectedTweet?.objectForKey("user") as! NSDictionary
+        
+        tweetText.text? = selectedTweet?.objectForKey("text") as! NSString as String
         tweetAuthorName.text? = userData.objectForKey("name") as! String
         
-        let imageURLString = userData.objectForKey(" profile_image_url") as! String
-        let imageURL = NSURL( string: imageURLString) as NSURL!
-        let imageData = NSData( contentsOfURL: imageURL!) as NSData!
-        dispatch_async( dispatch_get_main_queue()) {
-            self.tweetAuthorAvatar.image = UIImage( data: imageData!)
+        let imageURLString = userData.objectForKey("profile_image_url") as! String
+        let imageURL = NSURL(string: imageURLString) as NSURL!
+        let imageData = NSData(contentsOfURL: imageURL!) as NSData!
+        
+        dispatch_async(dispatch_get_main_queue()) {
+            self.tweetAuthorAvatar.image = UIImage(data: imageData!)
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
